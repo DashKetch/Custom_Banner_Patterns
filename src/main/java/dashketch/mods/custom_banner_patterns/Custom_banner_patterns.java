27562969ecafa50fbe9e2df252e845d02b7f4ab1
DashKetch <nateisthegreatgreat@gmail.com>
2026-01-19 10:39:47 -0800

    added pattern loader block

--- src/main/java/dashketch/mods/custom_banner_patterns/Custom_banner_patterns.java
+++ src/main/java/dashketch/mods/custom_banner_patterns/Custom_banner_patterns.java
@@ -1,14 +1,15 @@
 package dashketch.mods.custom_banner_patterns;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -21,6 +22,7 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -30,77 +32,61 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Custom_banner_patterns.MODID)
 public class Custom_banner_patterns {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "custom_banner_patterns";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Items which will all be registered under the "custom_banner_patterns" namespace
+
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "custom_banner_patterns" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final DeferredBlock<Block> PATTERN_LOADER = BLOCKS.register("pattern_loader",
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK)));
+
+    public static final DeferredItem<BlockItem> PATTERN_LOADER_ITEM = ITEMS.register("pattern_loader",
+            () -> new BlockItem(PATTERN_LOADER.get(), new Item.Properties()));
 
     public static final DeferredItem<Item> EMPTY_PATTERN = ITEMS.registerSimpleItem("empty_banner_pattern", new Item.Properties().stacksTo(16));
 
-    // Creates a creative tab with the id "custom_banner_patterns:custom_banners"
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CUSTOM_BANNERS = CREATIVE_MODE_TABS.register("custom_banners", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.custom_banner_patterns")).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> EMPTY_PATTERN.get().getDefaultInstance()).displayItems((parameters, output) -> {
-        output.accept(EMPTY_PATTERN.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-    }).build());
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CUSTOM_BANNERS = CREATIVE_MODE_TABS.register("custom_banners",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.custom_banner_patterns"))
+                    .withTabsBefore(CreativeModeTabs.COMBAT)
+                    .icon(() -> EMPTY_PATTERN.get().getDefaultInstance())
+                    .displayItems((parameters, output) -> {
+                        output.accept(EMPTY_PATTERN.get());
+                        output.accept(PATTERN_LOADER_ITEM.get());
+                    }).build());
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public Custom_banner_patterns(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so items get registered
+        // 4. Register the BLOCKS register to the bus
+        BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
         CREATIVE_MODE_TABS.register(modEventBus);
 
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (Custom_banner_patterns) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-
-        // Call the create directory function
         createPatternDirectory();
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock) LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-
-        LOGGER.info("{}{}", Config.magicNumberIntroduction, Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
-    // use SubscribeEvent and let the Event Bus discover methods to call
+
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
     }
 
-    // use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
-    
-    // Create directory for banner patterns
+
     private void createPatternDirectory() {
         Path patternPath = FMLPaths.CONFIGDIR.get().resolve("custom_patterns");
         try {
